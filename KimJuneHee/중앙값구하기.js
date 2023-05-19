1db951a9517d722f71d33a90db4b81352c79ce90
junehee <dev.junehee@gmail.com>
2023-05-19 15:00:44 +0900

    [PGM] 중앙값 구하기 / Level 0 / 4분

--- KimJuneHee/중앙값구하기.js
+++ KimJuneHee/중앙값구하기.js
@@ -1,7 +1,7 @@
 // 변수 할당 O
 function solution(array) {
   let newArr = array.sort((a, b) => a - b);
-  return newArr[Math.floor(array.length / 2)];
+  return newArr[Math.floor(newArr.length / 2)];
 }
 
 // 변수 할당 X
