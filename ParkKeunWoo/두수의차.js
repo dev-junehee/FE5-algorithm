e0811121ae8aad5c5673fd489de9085623def162
SpeedGear <oymin123@naver.com>
2023-05-25 00:36:32 +0900

    피자나눠먹기

--- ParkKeunWoo/두수의차.js
+++ ParkKeunWoo/두수의차.js
@@ -31,3 +31,45 @@ function solution(angle) {
       return 4    
   }   
 }
+
+// 두수의 나눗셈
+
+function solution(num1, num2) {
+  var answer = 0;
+  return parseInt(num1 / num2 * 1000);
+}
+
+//피자나눠먹기 1
+function solution(n) {
+  return n % 7 === 0 ? n / 7 : Math.floor(n / 7 )+1 ;
+}
+//피자나눠먹기 2
+function solution( n ) {
+	let pizza = 6;
+	while ( true ) {
+		if ( pizza % n === 0 ) {
+			break;
+		}
+		pizza += 6;
+	}
+	return pizza / 6;
+}
+//피자나눠먹기 3
+
+function solution(slice, n) {
+  var answer = 0;
+  answer = Math.floor( (n - 1) / slice) + 1;
+  return answer;
+}
+
+//배열의 평균값
+
+function solution(numbers) {
+  var answer = 0;
+  for(i = 0; i < numbers.length; i++) {
+      answer += numbers[i]
+  }
+  answer = answer / numbers.length
+  
+  return answer;
+}
