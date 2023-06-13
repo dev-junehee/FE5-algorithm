2a711f8b7c942273bd14e068822d1d58b0a3be09
ChoEun-Sang <wh94es@gmail.com>
2023-06-13 10:17:57 +0900

    [PGM] 가위바위보/ Lv.0 / 10분

---
+++ ChoEunSang/가위바위보.js
@@ -0,0 +1,15 @@
+function solution(rsp) {
+  let arr = rsp.split('') 
+  let answer = ''
+  for(i=0; i<arr.length; i++) {
+      if (arr[i] === "2") {
+          answer += "0"
+      } else if ( arr[i] === "0") {
+          answer += "5"
+      } else {
+          answer += "2"
+      }   
+  }
+  return answer
+}
+
